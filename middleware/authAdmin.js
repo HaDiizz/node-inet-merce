389749bc5d97ca7c89e6c54ebf99e3e1aecd8886
HaDiizz <nattapol.sh@hotmail.com>
2024-06-21 11:05:29 +0700

    fix user unauthor authAdmin

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -11,9 +11,11 @@ const authAdmin = async (req, res, next) => {
     return res.status(403).json({ message: "Forbidden", error: true });
 
   const user = await User.findOne({ _id: decoded._id });
-  if (user.role !== "admin")
+  if (!user)
+    return res.status(401).json({ message: "Unauthorized", error: true });
+  if (user?.role !== "admin")
     return res
-      .status(401)
+      .status(403)
       .json({ message: "Permission is denied", error: true });
   req.user = user.toObject();
   next();
